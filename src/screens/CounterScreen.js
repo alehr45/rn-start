7f0acc923f7ef268dabbb6cbcf460b5c1481ba69
Alex Lehr <alexsmacbook@Alexs-MacBook-Air.local>
2022-04-26 21:42:55 -0500

    useReducer learned

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,19 +1,26 @@
-import React,   { useState } from "react";
+import React, { useReducer } from "react";
 import { View, Text, StyleSheet, Button } from "react-native"
 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'increase_number':
+            return { ...state, counter: state.counter + action.payload }
+        case 'decrease_number':
+            return { ...state, counter: state.counter - action.payload }
+        default:
+            return;
+    }
+};
 
 const CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
+    const [state, dispatch] = useReducer(reducer, { counter: 0 })
+
 
     return (
-        <View style={{padding: 20}}>
-            <Button title="Increase Number" onPress={() => {
-                setCounter(counter + 1)
-            }} />
-            <Button title="Decrease Number" onPress={() => {
-                 setCounter(counter - 1)
-            }} />
-            <Text style={{textAlign: "center", paddingTop: 20}}>Current Count: {counter}</Text>
+        <View style={{ paddingTop: 50 }}>
+            <Button title="Increase Number" onPress={() => { dispatch({ type: 'increase_number', payload: 1 }) }} />
+            <Button title="Decrease Number" onPress={() => { dispatch({ type: 'decrease_number', payload: 1 }) }} />
+            <Text style={{ textAlign: "center", paddingTop: 20 }}>Current Count: {state.counter} </Text>
         </View>
     )
 }
---
+++ src/screens/TextScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native"
+
+
+const TextScreen = () => {
+    return (
+        <Text>TextScreen</Text>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default TextScreen;
